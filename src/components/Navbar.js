09387b8c5bbf8127a07dac3d063293b477f4c347
Teguh Ary <teguhary36@gmail.com>
2021-10-04 01:21:02 +0700

    edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,21 +5,21 @@ function Navbar(props) {
     return (
         <div>
             <nav className="navbar navbar-expand-xl navbar-light bg-light">
-                <div className="container-fluid m-3">
-                    <NavLink className="navbar-brand mx-auto" to="/">
+                <div className="container-fluid m-2">
+                    <NavLink className="navbar-brand mr-auto" to="/">
                         <a href="/" className="logo">
                             <img className="logo" src="http://placekitten.com/50/50"></img>
                         </a>
                     </NavLink>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="collapse navbar-collapse w-100" id="navbarNavAltMarkup">
                         <div className="navbar-nav mx-auto">
                             <NavLink exact className="nav-link mx-3" to="/">Home</NavLink>
                             <NavLink className="nav-link mx-3" to="/shop" >Shop</NavLink>
                             <NavLink className="nav-link mx-3" to="/contact-us">Contact Us</NavLink>
                         </div>
                     </div>
-                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-                        <div className="navbar-nav mx-0">
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ml-auto">
                             <NavLink className="nav-link mx-3" to="/">Cart</NavLink>
                             <NavLink className="nav-link mx-3" to="/">User</NavLink>
                             <NavLink className="nav-link mx-3" to="/">Search</NavLink>
